835b8e7457a76c4129982f071f17f2ef926a4b25
Farhaz Alam <dev_farhaz@arianus.in>
2020-08-29 13:21:43 +0530

    first commit

---
+++ app/src/main/java/com/example/composefirst/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.composefirst
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.example.composefirst.ui.ComposeFirstTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeFirstTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello Farhaz!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeFirstTheme {
+        Greeting("Android")
+    }
+}
